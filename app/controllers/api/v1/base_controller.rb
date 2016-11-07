743e82cb461ca8301004cbf10c03a4969d637929
Sunny Sakjiraphong <s@sunny.in.th>
2016-11-08 01:44:59 +0700

    Add api error

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session
 
+
+  def api_error(status: 500, errors: [])
+    head status: status and return if errors.empty?
+    render json: errors.to_json, status: status
+  end
+
 end
