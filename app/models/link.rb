57bf3729a7af4bee3647c495e04cb86947073029
Somchok Sakjiraphong <sunny@omise.co>
2016-11-07 19:27:42 +0700

    Implemented #generate_slug

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,13 @@
 class Link < ApplicationRecord
+  validates :source_url, presence: true
+  validates :slug, presence: true,
+
+  before_validation :generate_slug, on: :create
+
+  def generate_slug
+    begin
+      self.slug = SecureRandom.hex(3)
+    end while self.class.exists?(slug: slug)
+  end
+
 end
