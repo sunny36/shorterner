8b558941766ae64c3c143134764330cf56e8defc
Sunny Sakjiraphong <s@sunny.in.th>
2016-11-08 02:49:41 +0700

    Add url to json response

--- app/serializers/api/v1/link_serializer.rb
+++ app/serializers/api/v1/link_serializer.rb
@@ -1,3 +1,8 @@
 class Api::V1::LinkSerializer < Api::V1::BaseSerializer
-  attributes :id, :source_url, :slug, :clicks
+  attributes :id, :source_url, :slug, :clicks, :url
+
+
+  def url
+    ENV["DEFAULT_HOST"] || "http://localhost:300/#{object.slug}"
+  end
 end
